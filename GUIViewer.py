1ff81946252c944a38e09c7bf1c8e2ffde464efc
hf <moyuejian@outlook.com>
2020-01-19 02:19:55 +0800

    finish general UI design

--- GUIViewer.py
+++ GUIViewer.py
@@ -19,6 +19,43 @@ parser.add_argument('--host', type=str, default='127.0.0.1')
 args = parser.parse_args()
 host = args.host
 
+app = QtGui.QApplication([])
+lw = pg.LayoutWidget()
+lw.setWindowTitle('Camera Viewer')
+view = pg.GraphicsView()
+lw.addWidget(view, col=0)
+lg = pg.GraphicsLayout()
+view.setCentralItem(lg)
+view.show()
+
+vb = lg.addViewBox(lockAspect=True, invertY=True)
+# Create initial data
+data = np.random.normal(size=(1024,1024))
+img = pg.ImageItem( data )
+vb.addItem(img)
+
+# Create histogram and lut in graphics layout
+lut = pg.HistogramLUTItem()
+lut.setImageItem(img)
+lut.setHistogramRange(0, 50000)
+lg.addItem(lut)
+
+li = pg.LayoutWidget()
+lw.addWidget(li, row=1)
+# Checkbox
+auto_checkbox = QtGui.QCheckBox("Auto Level")
+auto_checkbox.setChecked(True)
+li.addWidget(auto_checkbox, 0, 0)
+# Connect
+auto_checkbox = QtGui.QCheckBox("Connect")
+auto_checkbox.setChecked(False)
+li.addWidget(auto_checkbox, 0, 1)
+# Connect
+auto_checkbox = QtGui.QCheckBox("Pixel")
+auto_checkbox.setChecked(False)
+li.addWidget(auto_checkbox, 0, 2)
+lw.show()
+
 connect = 1 # 0 mean connect, larger than 0 mean fail to connect
 def connect():
     try:
@@ -26,42 +63,6 @@ def connect():
         connect = 0
     except ConnectionRefusedError:
         print("No target to connect!")
-
-# Create graphics viewer
-view = pg.widgets.GraphicsView.GraphicsView()
-# Create Widget Layout
-layout = pg.LayoutWidget()
-layout.addWidget(view, 0, 0) #, 3, 1)
-layout.show()
-
-# Create graphics layout
-l_view = pg.GraphicsLayout()
-view.setCentralItem(l_view)
-# Create view box in graphics layout
-vb = pg.ViewBox(lockAspect=True, invertY=True)
-l_view.addItem(vb)
-# Create initial data
-data = np.random.normal(size=(1024,1024))
-img = pg.ImageItem( data )
-vb.addItem(img)
-# Create histogram and lut in graphics layout
-lut = pg.HistogramLUTItem()
-lut.setImageItem(img)
-lut.setHistogramRange(0, 50000)
-l_view.addItem(lut)
-#info_layout = layout.addLayout(colspan=3)
-# Checkbox
-auto_checkbox = QtGui.QCheckBox("Auto Level")
-auto_checkbox.setChecked(True)
-layout.addWidget(auto_checkbox, 1, 0)
-# Connect
-auto_checkbox = QtGui.QCheckBox("Connect")
-auto_checkbox.setChecked(False)
-layout.addWidget(auto_checkbox, 2, 0)
-# Connect
-auto_checkbox = QtGui.QCheckBox("Pixel")
-auto_checkbox.setChecked(False)
-layout.addWidget(auto_checkbox, 3, 0)
 
 #data = np.random.normal(size=(2048,2048))
 def update():
@@ -81,7 +82,6 @@ def update():
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(0.1) # Refersh each 16ms
-
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
