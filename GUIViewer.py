d65ad701072c1a8f71a5b00debda8a9d9f9d789b
hf <moyuejian@outlook.com>
2020-01-20 00:54:27 +0800

    finish

--- GUIViewer.py
+++ GUIViewer.py
@@ -47,13 +47,14 @@ auto_checkbox = QtGui.QCheckBox("Auto Level")
 auto_checkbox.setChecked(True)
 li.addWidget(auto_checkbox, 0, 0)
 # Connect
-auto_checkbox = QtGui.QCheckBox("Connect")
-auto_checkbox.setChecked(False)
-li.addWidget(auto_checkbox, 0, 1)
-# Connect
-auto_checkbox = QtGui.QCheckBox("Pixel")
-auto_checkbox.setChecked(False)
-li.addWidget(auto_checkbox, 0, 2)
+con_checkbox = QtGui.QCheckBox("Connect")
+con_checkbox.setChecked(False)
+con_checkbox.setCheckable(False)
+li.addWidget(con_checkbox, 0, 1)
+
+# Pixel Picker
+intensity_picker= QtGui.QLabel("Intensity")
+li.addWidget(intensity_picker, 0, 2)
 lw.show()
 
 connect = 1 # 0 mean connect, larger than 0 mean fail to connect
@@ -70,6 +71,7 @@ def update():
         data = viewer.recv_img()
     else:
         data = None
+        #data = np.random.normal(size=(2048,2048))
         #connect()
         #timer.start(10)
 
@@ -79,9 +81,24 @@ def update():
         else:
             img.setImage(data.T, clear=True, _callSync='off', autoLevels=False)
 
+"""
+Pick grey value from image
+"""
+def mouseMoved(evt):
+    pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+    if vb.sceneBoundingRect().contains(pos):
+        x = int(vb.mapSceneToView(pos).x()//1)
+        y = int(vb.mapSceneToView(pos).y()//1)
+        if x < 0 or x>data.shape[0]-1 or y<0 or y>data.shape[1]-1:
+            intensity_picker.setText("Out of Region")
+        else:
+            intensity_picker.setText("x=%d, y=%d, I=%d"%(x, y, data[x,y]))
+
+proxy = pg.SignalProxy(img.scene().sigMouseMoved, rateLimit=60, slot=mouseMoved)
+
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(0.1) # Refersh each 16ms
+timer.start(1) # Refersh each 16ms
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
